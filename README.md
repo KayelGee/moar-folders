1c2dfc4993873c97d39b8652c2f2991cf142d023
KayelGee <kayelGee2@gmail.com>
2020-10-23 11:35:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# FoundryVTT - Moar Folders
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/KayelGee/moar-folders?style=for-the-badge) 
+![GitHub Releases](https://img.shields.io/github/downloads/KayelGee/moar-folders/latest/total?style=for-the-badge) 
+![GitHub All Releases](https://img.shields.io/github/downloads/KayelGee/moar-folders/total?style=for-the-badge&label=Downloads+total)  
+
+**[Compatibility]**: *FoundryVTT* 0.7.0+  
+**[Systems]**: *any*  
+**[Languages]**: *English*  
+Allows increasing of the maximum folder depth.
+Use at your own risk, as this is removing a limitation set by core Foundry and can cause UI and performance problems.
+
+## Installation
+
+1. moar-folders using manifest URL: https://raw.githubusercontent.com/KayelGee/moar-folders/master/module.json
+2. While loaded in World, enable **_Moar Folders"_** module.
+
+## Usage
+
+Go into module settings and set your preferred folder depth.
+Reload with F5 for changes to take effect.
+
+![](moar-folders.gif)
+
+## Contact
+
+If you wish to contact me for any reason, reach me out on Discord using my tag: `KayelGee#5241`
---
+++ moar-folders.js
@@ -0,0 +1,54 @@
+(async () => {
+	const moduleName = "moar-folders";
+	class MoarFolders {
+		static registerSettings(){
+			game.settings.register(moduleName, "folderDepth", {
+				name: game.i18n.localize("MF.SettingFolderDepth"),
+				hint: game.i18n.localize("MF.SettingFolderDepthHint"),
+				scope: "world",
+				config: true,
+				type: Number,
+				default: 3
+			  });
+		}
+
+		static initialize(){
+			MoarFolders.registerSettings();
+		
+			CONST.FOLDER_MAX_DEPTH = game.settings.get(moduleName, "folderDepth") || 3;
+
+			//Monkey patch activateListeners so the create folder button is only removed from the final folder
+			var oldactivateListeners= SidebarDirectory.prototype.activateListeners;
+			SidebarDirectory.prototype.activateListeners= function() {
+				let addFunc= function(html){
+    				html.find(".folder .folder .folder .create-folder").addClass("create-folder-moar"); 
+    				html.find(".folder .folder .folder .create-folder").removeClass("create-folder"); 
+				}
+				let removeFunc= function(html){
+    				html.find(".folder .folder .folder .create-folder-moar").addClass("create-folder"); 
+					html.find(".folder .folder .folder .create-folder-moar").removeClass("create-folder-moar"); 
+
+    				html.find(".folder ".repeat(CONST.FOLDER_MAX_DEPTH)+".create-folder").remove(); 
+					if ( game.user.isGM ) html.find('.create-folder').click(ev => this._onCreateFolder(ev));
+				}
+				addFunc.apply(this, arguments);
+				const result = oldactivateListeners.apply(this, arguments);
+				removeFunc.apply(this, arguments);
+				return result;
+			};
+			//Reload all directories after some time for the changes to take effect
+			setTimeout(()=>{
+				for (const key in ui) {
+					if (ui.hasOwnProperty(key)) {
+						const ui_elem = ui[key];
+						if(ui_elem.constructor.name.includes("Directory")) ui_elem.render(true);
+
+					}
+				}
+			}, 1000);
+		}
+
+
+	}
+	Hooks.on('canvasReady', () => MoarFolders.initialize());
+})();
